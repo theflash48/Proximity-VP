e2d885f89f29cfac395489899a0495177bc711b0
sinnerbie <165671232+sinnerbie@users.noreply.github.com>
2025-10-25 12:12:05 +0200

    Daño visual HUD

--- Proximity-VP/Assets/Scripts/Player/PlayerHUD.cs
+++ Proximity-VP/Assets/Scripts/Player/PlayerHUD.cs
@@ -3,5 +3,22 @@ using UnityEngine.UI;
 
 public class PlayerHUD : MonoBehaviour
 {
-    
+    [SerializeField] private Image _cHUD;
+    [SerializeField] private Image _cReloadBar;
+    [SerializeField] private Image _cInvisibility;
+
+    float harm = 10;
+    [ContextMenu("Try")]
+    public void test()
+    {
+        harm -= 9;
+        _fHealthUI(harm, 10);
+    }
+
+    public void _fHealthUI(float cur, float max)
+    {
+        float damage = cur / max;
+        Debug.Log(damage);
+        _cHUD.color = new Color (255, 1 - damage, 1 - damage, 255);
+    }
 }
