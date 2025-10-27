416731a273a899d9eab1b773afe13fff181b95b9
Sertame <serdina2806@gmail.com>
2025-10-27 13:58:24 +0100

    Timer Local

---
+++ Proximity-VP/Assets/Scripts/UI/TimerLocal.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class TimerLocal : MonoBehaviour
+
+{
+
+    [SerializeField] Text timerText;
+    [SerializeField]float remainingTime;
+    // Update is called once per frame
+    void Update()
+    {
+        if (remainingTime > 0)
+        {
+            remainingTime -=Time.deltaTime;
+        }
+        else if (remainingTime <= 0)
+        {
+            remainingTime = 0;
+        }
+        
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
