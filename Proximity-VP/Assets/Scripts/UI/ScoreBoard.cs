58673f2afa97d5c8996f1cffb348660c9fa1348b
sinnerbie <165671232+sinnerbie@users.noreply.github.com>
2025-11-03 17:42:07 +0100

    sore

--- Proximity-VP/Assets/Scripts/UI/ScoreBoard.cs
+++ Proximity-VP/Assets/Scripts/UI/ScoreBoard.cs
@@ -9,6 +9,11 @@ public class ScoreBoard : MonoBehaviour
     List<PlayerControllerLocal> localPlayers = new List<PlayerControllerLocal>();
     List<PlayerControllerOnline> onlinePlayers = new List<PlayerControllerOnline>();
 
+    List<GameObject> playerOrder = new List<GameObject>();
+
+    public GameObject[] scoreBanners;
+    public GameObject scorePanel;
+
     private void OnEnable()
     {
         TimerLocal.onTryStartGame += LocatePlayers;
@@ -39,6 +44,10 @@ public class ScoreBoard : MonoBehaviour
         {
             Debug.LogError("There are no players in the game");
             return;
+        }
+
+        for (int i = 0; i < players.Length; i++) {
+            playerOrder.Add(players[i]);
         }
     }
 
@@ -97,6 +106,91 @@ public class ScoreBoard : MonoBehaviour
                 }
             }
             
+        }
+    }
+
+    int playersName;
+    public void PrintScores()
+    {
+        if (localPlayers[0].GetComponent<PlayerControllerLocal>() != null)
+        {
+            for (int i = 0; i < localPlayers.Count; i++)
+            {
+                GameObject ban = Instantiate(scoreBanners[0]);
+                ban.transform.SetParent(scorePanel.transform);
+                switch (i)
+                {
+                    case 0:
+                        
+                        for (int b = 0; b < playerOrder.Count; b++) {
+                            if (localPlayers[0].score == playerOrder[b].GetComponent<PlayerControllerLocal>().score)
+                                playersName = b + 1;
+                        }
+                        ban.GetComponent<ScoreBanner>().UpdateBanner("1st", "Player " + playersName.ToString(), localPlayers[0].score.ToString());
+                        break;
+                    case 1:
+                        for (int b = 0; b < playerOrder.Count; b++) {
+                            if (localPlayers[1].score == playerOrder[b].GetComponent<PlayerControllerLocal>().score)
+                                playersName = b + 1;
+                        }
+                        ban.GetComponent<ScoreBanner>().UpdateBanner("2nd", "Player " + playersName.ToString(), localPlayers[1].score.ToString());
+                        break;
+                    case 2:
+                        for (int b = 0; b < playerOrder.Count; b++) {
+                            if (localPlayers[2].score == playerOrder[b].GetComponent<PlayerControllerLocal>().score)
+                                playersName = b + 1;
+                        }
+                        ban.GetComponent<ScoreBanner>().UpdateBanner("3rd", "Player " + playersName.ToString(), localPlayers[2].score.ToString());
+                        break;
+                    case 3:
+                        for (int b = 0; b < playerOrder.Count; b++) {
+                            if (localPlayers[3].score == playerOrder[b].GetComponent<PlayerControllerLocal>().score)
+                                playersName = b + 1;
+                        }
+                        ban.GetComponent<ScoreBanner>().UpdateBanner("2nd", "Player " + playersName.ToString(), localPlayers[3].score.ToString());
+                        break;
+                }
+            }
+        }
+        else if (onlinePlayers[0].GetComponent<PlayerControllerOnline>() != null)
+        {
+            for (int i = 0; i < onlinePlayers.Count; i++)
+            {
+                GameObject ban = Instantiate(scoreBanners[0]);
+                ban.transform.SetParent(scorePanel.transform);
+                switch (i)
+                {
+                    case 0:
+                        
+                        for (int b = 0; b < playerOrder.Count; b++) {
+                            if (onlinePlayers[0].score == playerOrder[b].GetComponent<PlayerControllerOnline>().score)
+                                playersName = b + 1;
+                        }
+                        ban.GetComponent<ScoreBanner>().UpdateBanner("1st", "Player " + playersName.ToString(), onlinePlayers[0].score.ToString());
+                        break;
+                    case 1:
+                        for (int b = 0; b < playerOrder.Count; b++) {
+                            if (onlinePlayers[1].score == playerOrder[b].GetComponent<PlayerControllerOnline>().score)
+                                playersName = b + 1;
+                        }
+                        ban.GetComponent<ScoreBanner>().UpdateBanner("2nd", "Player " + playersName.ToString(), onlinePlayers[1].score.ToString());
+                        break;
+                    case 2:
+                        for (int b = 0; b < playerOrder.Count; b++) {
+                            if (onlinePlayers[2].score == playerOrder[b].GetComponent<PlayerControllerOnline>().score)
+                                playersName = b + 1;
+                        }
+                        ban.GetComponent<ScoreBanner>().UpdateBanner("3rd", "Player " + playersName.ToString(), onlinePlayers[2].score.ToString());
+                        break;
+                    case 3:
+                        for (int b = 0; b < playerOrder.Count; b++) {
+                            if (onlinePlayers[3].score == playerOrder[b].GetComponent<PlayerControllerOnline>().score)
+                                playersName = b + 1;
+                        }
+                        ban.GetComponent<ScoreBanner>().UpdateBanner("2nd", "Player " + playersName.ToString(), onlinePlayers[3].score.ToString());
+                        break;
+                }
+            }
         }
     }
 }
